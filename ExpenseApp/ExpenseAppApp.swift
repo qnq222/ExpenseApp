e1ab6f65147abe2cdb4e96a01aa79f0143c170db
qnq222 <48182243+qnq222@users.noreply.github.com>
2022-05-22 10:00:42 +0300

    Initial Commit

---
+++ ExpenseApp/ExpenseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseAppApp.swift
+//  ExpenseApp
+//
+//  Created by Ayman AbuMutair on 22/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
